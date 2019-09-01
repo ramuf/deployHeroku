4095ce041f5c3df5d0e27f8b1ff15698c5319318
ramuf <48536005+ramuf@users.noreply.github.com>
2019-09-01 15:48:32 +0100

    Add files via upload

--- run.py
+++ run.py
@@ -10,6 +10,10 @@ server.secret_key = os.environ.get('secret_key', 'secret')
 app = dash.Dash(name = __name__, server = server)
 app.config.supress_callback_exceptions = True
 
+path = 'data/' 
+files = os.listdir()
+files_txt = [i for i in files if i.endswith('.csv')]
+
 app.layout = html.Div(children=[
     html.H1(children='Hello Dash'),
     html.Div(children='''
@@ -30,3 +34,10 @@ app.layout = html.Div(children=[
     dcc.Input(id='my-id', value='initial value', type="text"),
     html.Div(id='my-div')
 ])
+
+@app.callback(
+    Output(component_id='my-div', component_property='children'),
+    [Input(component_id='my-id', component_property='value')]
+)
+def update_output_div(input_value):
+    return 'You\'ve entered "{}"'.format(input_value)
