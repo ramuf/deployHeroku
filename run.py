efd8172d512ab437c9b77d0ce6ff75d65ef6f833
ramuf <48536005+ramuf@users.noreply.github.com>
2019-09-01 15:32:42 +0100

    Update run.py

--- run.py
+++ run.py
@@ -30,10 +30,3 @@ app.layout = html.Div(children=[
     dcc.Input(id='my-id', value='initial value', type="text"),
     html.Div(id='my-div')
 ])
-
-@app.callback(
-    Output(component_id='my-div', component_property='children'),
-    [Input(component_id='my-id', component_property='value')]
-)
-def update_output_div(input_value):
-    return 'You\'ve entered "{}"'.format(input_value)
