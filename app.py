f1ceaa28db8f361dbfc4624aa10cdccf74a5cb4c
Alex <ramusga@gmail.com>
2019-09-05 19:48:49 +0000

    Update app.py

--- app.py
+++ app.py
@@ -71,14 +71,14 @@ app.layout = html.Div(children=[
     dash.dependencies.Output('columns-select', 'children'),
     #dash.dependencies.Output('plot-graph', 'figure'),
     [dash.dependencies.Input('my-dropdown', 'value')])
-    def columns_select(value):
-        df = pd.read_csv(path + value)
-        return {
-            html.P(
-                "Select the file to plot."
-            ),
+def columns_select(value):
+    df = pd.read_csv(path + value)
+    return {
+        
+        "Select the file to plot."
+            
  
-        } 
+    } 
 
 
 
