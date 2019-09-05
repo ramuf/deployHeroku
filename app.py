e6773825dfb9bb3b6820f15828f80cce9fc66f96
ramuf <48536005+ramuf@users.noreply.github.com>
2019-09-05 15:39:26 +0100

    Update app.py

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ app.layout = html.Div(children=[
     #dash.dependencies.Output('plot-graph', 'figure'),
     [dash.dependencies.Input('my-dropdown2', 'value')])
 def update_graph(value):
-    df = pd.read_csv(value)
+    #df = pd.read_csv(value)
     return 'You have selected "{}"'.format(value)
     
 
