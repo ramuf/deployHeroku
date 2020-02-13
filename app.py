a09ef59f6cb57ac0ae6ff90623d992438518c038
ramuf <48536005+ramuf@users.noreply.github.com>
2020-02-13 13:50:41 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,108 +1,29 @@
+import os
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
-from flask import Flask
-import pandas as pd
-import os
-#external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-server = Flask(__name__)
-server.secret_key = os.environ.get('secret_key', 'secret')
-app = dash.Dash(name = __name__, server = server)
-app.config.supress_callback_exceptions = True
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-path = 'data/' 
-files = os.listdir(path)
-files_txt = [i for i in files if i.endswith('.csv')]
+server = app.server
 
-
-#app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
-
-    html.Div(children='''
-        Dash: A web application framework for Python.
-        By Ramuf
-    '''),
-
-    html.Br(),
-    
-    html.Div(
-        children=[
-            html.P(
-                "Select the file to plot."
-            ),
-
-            dcc.Dropdown(
-                id="my-dropdown",
-                options=list({"label": file, "value": file} for file in files_txt),
-                placeholder="Select a file",
-                clearable=False,
-                #value=files_txt[1],
-            ),
-
-            html.P(
-                "Select the columns to plot."
-            ),
-
-            dcc.Dropdown(
-                id='columns-dropdown',
-                #disabled=True,
-                #multi=True,
-            ),
-
-        ],
-        style={'width': '30%'},       
+app.layout = html.Div([
+    html.H2('Hello World'),
+    dcc.Dropdown(
+        id='dropdown',
+        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
+        value='LA'
     ),
-
- #   html.Div( id='columns-select' ),
-    
-    dcc.Graph(
-        id='example-graph',
-        figure={
-            'data': [
-                #{'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                {'x': ['WHITE', 'BLACK', 'ASIAN'], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-            ],
-            'layout': {
-                'title': 'Dash Data Visualization'
-            }
-        }
-    ),
-    
-    dcc.Graph(
-        id='plot-graph'        
-    )
-
-
+    html.Div(id='display-value')
 ])
 
-@app.callback(
- #   dash.dependencies.Output('columns-select', 'children'),
-    dash.dependencies.Output('columns-dropdown', 'options'),
-    #dash.dependencies.Output('plot-graph', 'figure'),
-    [dash.dependencies.Input('my-dropdown', 'value')])
-def columns_select(value):
-    df = pd.read_csv(path + value)
-    #return 'You have selected "{}"'.format(path + value)
-    return [{"label": column, "value": column} for column in df.columns]
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
+@app.callback(dash.dependencies.Output('display-value', 'children'),
+              [dash.dependencies.Input('dropdown', 'value')])
+def display_value(value):
+    return 'You have selected "{}"'.format(value)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
